0093e98d14e48857b9fd3147b65e21754c50217c
mdatsev <martin.datsev.p@gmail.com>
2018-01-30 16:31:06 +0200

    refactor

--- main.js
+++ main.js
@@ -21,107 +21,85 @@ document.querySelectorAll('a[href*="#"]').forEach(function (el) {
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
     var img = new Image();
-    img.addEventListener('load', function () {
 
-        const hCount = 25;
-        const vCount = 10;
-        const spread = 2000;
-        const speed = 1 - 0.10;
-        const angleSpread = 10;
-        const epsilon =  0.0015;
-        let points = [];
-        const signedRnd = _ => 0.5 - Math.random();
-        for (let i = 0; i < vCount; i++) {
-            points[i] = [];
-            for (let j = 0; j < hCount; j++) {
-                points[i][j] = {
-                    x: canvas.width / hCount * (j + ((j == 0 || j == hCount - 1) ? 0 : Math.random())),
-                    y: canvas.height / vCount * (i + ((i == 0 || i == vCount - 1) ? 0 : Math.random())),
-                    offset1x: signedRnd() * spread,
-                    offset1y: signedRnd() * spread,
-                    offset2x: signedRnd() * spread,
-                    offset2y: signedRnd() * spread,
-                    angle1: signedRnd() * angleSpread,
-                    angle2: signedRnd() * angleSpread
-                };
-            }
+    const hCount = 20;
+    const vCount = 12;
+    const xspread = 2000;
+    const yspread = 0;
+    const speed = 1 - 0.10;
+    const angleSpread = 0;
+    const epsilon = 0.1;
+    let points = [];
+    const signedRnd = _ => 0.5 - Math.random();
+    for (let i = 0; i < vCount; i++) {
+        points[i] = [];
+        for (let j = 0; j < hCount; j++) {
+            points[i][j] = {
+                x: canvas.width / hCount * (j + ((j == 0 || j == hCount - 1) ? 0 : Math.random())),
+                y: canvas.height / vCount * (i + ((i == 0 || i == vCount - 1) ? 0 : Math.random())),
+                offset1x: signedRnd() * xspread,
+                offset1y: signedRnd() * yspread,
+                offset2x: signedRnd() * xspread,
+                offset2y: signedRnd() * yspread,
+                angle1: signedRnd() * angleSpread,
+                angle2: signedRnd() * angleSpread
+            };
         }
+    }
+
+    img.addEventListener('load', function () {
 
         const centerx = canvas.width / 2 - img.width / 2;
         const centery = canvas.height / 2 - img.height / 2;
 
-        function draw()
-        {
+        function drawSegment(image, p1, p2, p3, offsetx, offsety, angle) {
+            ctx.save();
+            if (offsetx || offsety) {
+                ctx.translate(offsetx, offsety);
+            }
+
+            if (angle) {
+                center = {
+                    x: (p1.x + p2.x + p3.x) / 3,
+                    y: (p1.y + p2.y + p3.y) / 3
+                };
+                ctx.translate(center.x, center.y)
+                ctx.rotate(angle);
+                ctx.translate(-center.x, -center.y)
+            }
+
+            ctx.beginPath();
+            ctx.moveTo(p1.x, p1.y);
+            ctx.lineTo(p2.x, p2.y);
+            ctx.lineTo(p3.x, p3.y);
+            ctx.clip();
+            ctx.drawImage(image, centerx, centery);
+            ctx.restore();
+
+        }
+
+        function draw() {
             let done = true;
-            ctx.clearRect(0, 0, canvas.width, canvas.height);                                
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
             for (let i = 0; i < vCount - 1; i++) {
                 for (let j = 0; j < hCount - 1; j++) {
-                        const p1 = points[i][j];
+                    const p1 = points[i][j];
+                    const p2 = points[i + 1][j];
+                    const p3 = points[i][j + 1];
+                    const p4 = points[i + 1][j + 1];
 
-                        if(p1.done)
-                        {
-                            continue;
-                        }
-                        else if(Math.abs(p1.offset1x) < epsilon && Math.abs(p1.offsety) < epsilon
-                                && Math.abs(p1.offset2x) < epsilon && Math.abs(p1.offset2y) < epsilon)
-                        {
-                            p1.done = true;
-                            p1.offset1x = p1.offset1y = p1.offset2x = p1.offset2y = 0;
-                        }
-                        else
-                        {
-                            done = false;
-                        }
+                    drawSegment(img, p1, p2, p3, p1.offset1x, p1.offset1y, p1.angle1);
+                    drawSegment(img, p2, p3, p4, p1.offset2x, p1.offset2y, p1.angle2);
 
-                        const p2 = points[i + 1][j];
-                        const p3 = points[i][j + 1];
-                        const p4 = points[i + 1][j + 1];
 
-                        p1.center1 = p1.center1 || (p1.center1 = { x: (p1.x + p2.x + p3.x) / 3, y: (p1.y + p2.y + p3.y) / 3});
-                        p1.center2 = p1.center2 || (p1.center2 = { x: (p2.x + p3.x + p4.x) / 3, y: (p2.y + p3.y + p4.y) / 3});
-                        
-                        ctx.save();
-                        ctx.translate(p1.offset1x, p1.offset1y);
-                        
-                        ctx.translate(p1.center1.x, p1.center1.y)
-                        ctx.rotate(p1.angle1);    
-                        ctx.translate(-p1.center1.x, -p1.center1.y)
-                        
-                        
-                        ctx.beginPath();                                                               
-                        ctx.moveTo(p1.x, p1.y);
-                        ctx.lineTo(p2.x, p2.y);
-                        ctx.lineTo(p3.x, p3.y);
-                        ctx.clip();                    
-                        ctx.drawImage(img, centerx, centery);
-                        ctx.restore();
-
-                        ctx.save();   
-                        ctx.translate(p1.offset2x, p1.offset2y);   
-                        
-                        ctx.translate(p1.center2.x, p1.center2.y)
-                        ctx.rotate(p1.angle2);    
-                        ctx.translate(-p1.center2.x, -p1.center2.y)   
-
-                          
-                        ctx.beginPath();   
-                        ctx.moveTo(p2.x, p2.y);
-                        ctx.lineTo(p3.x, p3.y);
-                        ctx.lineTo(p4.x, p4.y);
-                        ctx.clip();                    
-                        ctx.drawImage(img, centerx, centery);                        
-                        ctx.restore();
-                        p1.offset1x *= speed;
-                        p1.offset1y *= speed;
-                        p1.offset2x *= speed;
-                        p1.offset2y *= speed;
-                        p1.angle1 *= speed;
-                        p1.angle2 *= speed;
+                    [
+                        "offset1x",
+                        "offset1y",
+                        "offset2x",
+                        "offset2y",
+                        "angle1",
+                        "angle2"].forEach(p => p1[p] *= Math.abs(p1[p]) > epsilon ? speed : 0)
                 }
-            }
-            if(done){
-                console.log("done");
-                return;
             }
             window.requestAnimationFrame(draw);
         }
